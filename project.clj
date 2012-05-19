63a241cb8faac1d4ff401baea9e9be4154359dc7
Swaroop C H <swaroop@swaroopch.com>
2012-05-19 09:21:50 +0530

    Adding deps - hiccup, monger, joda time, data.json
    Looks like monger needs data.json because I got errors looking for that class, it doesn't say so in the docs though.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject isbnnetinclj "0.1.0-SNAPSHOT"
             :description "A quick way to find the online prices for a book in India"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.2.1"]]
+                           [org.clojure/data.json "0.1.3"]
+                           [noir "1.3.0-beta7"]
+                           [hiccup "1.0.0"]
+                           [com.novemberain/monger "1.0.0-beta6"]
+                           [joda-time "2.1"]]
             :main isbnnetinclj.server)
-
