c9e172bacb788171676bb9f8a1ce0d8e74099d88
Swaroop C H <swaroop@swaroopch.com>
2012-05-20 12:34:32 +0530

    Noting enlive selectors syntax page

--- src/isbnnetinclj/models/stores.clj
+++ src/isbnnetinclj/models/stores.clj
@@ -2,6 +2,7 @@
   [:require [net.cgrand.enlive-html :as html]
    [clojure.string :as string]])
 
+; http://enlive.cgrand.net/syntax.html
 (def sites
   {:flipkart
    {:url "http://www.flipkart.com/search.php?query=%s"
