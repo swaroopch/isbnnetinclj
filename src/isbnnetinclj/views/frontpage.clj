0092f4f73b86429514ab12b621d5f46261352167
Swaroop C H <swaroop@swaroopch.com>
2012-05-21 21:43:24 +0530

    Trying to get "/" path working

--- src/isbnnetinclj/views/frontpage.clj
+++ src/isbnnetinclj/views/frontpage.clj
@@ -84,4 +84,4 @@
 (defpage front-page-alt "/front"
   [& args]
   "FIXME Remove this : For debugging only, for some reason, on my Mac, my localhost:8080 redirects to a fixed website"
-  (front-page args))
+  (common/layout (front-page-content)))
