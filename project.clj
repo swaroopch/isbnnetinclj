8662d236d94c7e71ce05fa39deaa35c5ffd91626
Swaroop C H <swaroop@swaroopch.com>
2012-05-29 11:05:45 +0530

    Switching from Hiccup to Mustache (Stencil)

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [org.clojure/data.json "0.1.3"]
                            [noir "1.3.0-beta7"]
-                           [hiccup "1.0.0"]
-                           [hiccup-bootstrap "0.1.0"]
                            [com.novemberain/monger "1.0.0-beta6"]
                            [clj-time "0.4.2"]
-                           [enlive "1.0.0"]]
+                           [enlive "1.0.0"]
+                           [stencil "0.3.0-preview1"]]
             :main isbnnetinclj.server)
