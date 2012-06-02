d29c1d186770663e06b7234333fba2239a6f63ee
Swaroop C H <swaroop@swaroopch.com>
2012-06-02 08:57:19 +0530

    Simplified request logging

--- src/isbnnetinclj/views/book.clj
+++ src/isbnnetinclj/views/book.clj
@@ -1,19 +1,30 @@
 (ns isbnnetinclj.views.book
-  (:require [noir.request]
+  (:require [clojure.string :as string]
+            [noir.request]
             [stencil.core :as mus]
-            [isbnnetinclj.models.requestlog :as requestlog]
+            [monger.collection :as mc]
             [isbnnetinclj.utils :as utils])
   (:use [noir.core]
-        [isbnnetinclj.models.info :only [get-book-info]]
-        [isbnnetinclj.models.stores :only [prices-for-isbn]]))
+        [isbnnetinclj.models.info :only [book-info]]
+        [isbnnetinclj.models.stores :only [book-data]]))
+
+
+(def request-collection "request")
+
+(defn core-details-of-request
+  [request]
+  {:ip (get-in request [:headers :x-forwarded-for])
+   :user-agent (get-in request [:headers :user-agent])
+   :isbn (string/replace (get request :uri) "/" "")
+   :when (java.util.Date.)})
+
 
 (defpage "/:isbn" {:keys [isbn]}
-  (let [prices-log (prices-for-isbn isbn)
-        request-to-save (dissoc (noir.request/ring-request) :body)
-        book-info (get-book-info isbn)]
-    (requestlog/save-request-log-entry request-to-save)
+  (let [prices-log (book-data isbn)
+        info (book-info isbn)]
+    (mc/insert request-collection (core-details-of-request (noir.request/ring-request)))
     (mus/render-file "book.mustache" {:prices (:prices prices-log)
                                       :when-prices (utils/format-timestamp (:timestamp prices-log))
                                       :isbn isbn
-                                      :info (:info book-info)
+                                      :info (:info info)
                                       :title (or (get-in book-info [:info :title]) "isbn.net.in")})))
