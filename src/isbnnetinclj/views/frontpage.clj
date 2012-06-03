8eae8c7fba83e1cb32820b8e2a5fb19c7c0da177
Swaroop C H <swaroop@swaroopch.com>
2012-06-03 10:42:09 +0530

    Minor code formatting

--- src/isbnnetinclj/views/frontpage.clj
+++ src/isbnnetinclj/views/frontpage.clj
@@ -3,19 +3,21 @@
         [isbnnetinclj.models.stores :only [sites]])
   (:require [stencil.core :as mus]))
 
+
 (defn store-names
-    []
-    (sort (map name (keys sites))))
+  []
+  (sort (map name (keys sites))))
 
 (defn front-page-content
   []
   (mus/render-file "frontpage" {:title "isbn.net.in" :stores (store-names)}))
 
+
 (defpage front-page "/"
   [& args]
   (front-page-content))
 
+;; FIXME Remove this : For debugging only, for some reason, on my Mac, my localhost:8080 redirects to a fixed website
 (defpage front-page-alt "/front"
   [& args]
-  "FIXME Remove this : For debugging only, for some reason, on my Mac, my localhost:8080 redirects to a fixed website"
   (front-page-content))
