b8d11cc827e8e8794974182517d8c4699d9e9cf9
Swaroop C H <swaroop@swaroopch.com>
2012-06-03 19:28:39 +0530

    Redirect ISBN-10 to ISBN-13 page

--- src/isbnnetinclj/views/book.clj
+++ src/isbnnetinclj/views/book.clj
@@ -2,6 +2,7 @@
   (:require [timbre.core :as log]
             [clojure.string :as string]
             [noir.request]
+            [noir.response]
             [stencil.core :as mus]
             [monger.collection :as mc]
             [isbnnetinclj.utils :as utils])
@@ -27,23 +28,46 @@
   (sort-by :price (map (fn [[store-name price]]
                          {:name (name store-name)
                           :price price
-                          :url (format (get-in sites [store-name :url]) isbn)}) prices)))
+                          :url (format (get-in sites [store-name :url]) isbn)})
+                       prices)))
 
 
-(defn is-isbn-valid
+(defn is-isbn-10
   [isbn]
-  (or (re-matches #"^[0-9]{9}[0-9xX]$" isbn)
-      (re-matches #"^[0-9]{13}$" isbn)))
+  (re-matches #"^[0-9]{9}[0-9xX]$" isbn))
+
+
+(defn is-isbn-13
+  [isbn]
+  (re-matches #"^[0-9]{13}$" isbn))
+
+
+;; http://refactormycode.com/codes/33-isbn10-to-isbn13#refactor_257 
+(defn convert-isbn-10-to-13
+  [isbn]
+  (let [x (map (comp #(Integer/parseInt %) str) (to-array isbn))
+        sum-of-digits (+ 38
+                         (* 3 (+ (nth x 0) (nth x 2) (nth x 4) (nth x 6) (nth x 8)))
+                         (+ (nth x 1) (nth x 3) (nth x 5) (nth x 7)))
+        check-digit (mod (- 10 (mod sum-of-digits 10)) 10)
+        isbn-except-last-digit (.substring isbn 0 (- (.length isbn) 1))]
+    (str "978" isbn-except-last-digit check-digit)))
+
+
+(defn strip-dashes
+  [text]
+  (string/replace text "-" ""))
 
 
 (defpage book-page [:get ["/:isbn" :isbn #"[\d-]+[xX]?"]] {:keys [isbn]}
-  (let [isbn (string/replace isbn "-" "")]
-    (if (is-isbn-valid isbn)
-      ;; NOTE Launching price fetchers in background before starting info fetcher
-      (let [data (book-data isbn)
-            info (book-info isbn)]
-        (mc/insert request-collection (core-details-of-request (noir.request/ring-request)))
-        (mus/render-file "book" {:prices (convert-prices-for-display isbn (:price data))
-                                 :isbn isbn
-                                 :info (:info info)
-                                 :title (or (get-in info [:info :title]) "isbn.net.in")})))))
+  (let [isbn (strip-dashes isbn)]
+    (if (is-isbn-10 isbn)
+      (noir.response/redirect (str "/" (convert-isbn-10-to-13 isbn)) :permanent)
+      (if (is-isbn-13 isbn)
+        (let [data (book-data isbn) ; NOTE Launching price fetchers in background before starting info fetcher
+              info (book-info isbn)]
+          (mc/insert request-collection (core-details-of-request (noir.request/ring-request)))
+          (mus/render-file "book" {:prices (convert-prices-for-display isbn (:price data))
+                                   :isbn isbn
+                                   :info (:info info)
+                                   :title (or (get-in info [:info :title]) "isbn.net.in")}))))))
