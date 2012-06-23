61f03eeefa6a1c28d0cdb7023dd2bfefde56d6b5
Swaroop C H <swaroop@swaroopch.com>
2012-06-23 17:02:27 +0530

    Flush stats cache as well

--- src/isbnnetinclj/views/flush.clj
+++ src/isbnnetinclj/views/flush.clj
@@ -2,7 +2,8 @@
   (:require [timbre.core :as log]
             [noir.core :refer :all]
             [isbnnetinclj.models.info :refer [book-info-cache]]
-            [isbnnetinclj.models.stores :refer [book-data-cache book-in-progress-lock]]))
+            [isbnnetinclj.models.stores :refer [book-data-cache book-in-progress-lock]]
+            [isbnnetinclj.views.stats :refer [book-stats-cache]]))
 
 
 (defpage "/flush/:password" {:keys [password]}
@@ -11,5 +12,6 @@
       (reset! book-info-cache {})
       (reset! book-data-cache {})
       (reset! book-in-progress-lock {})
+      (reset! book-stats-cache {})
       "Flushed!")
     "You don't have permission"))
