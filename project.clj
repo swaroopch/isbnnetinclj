ff5db83f8b0b3186129bc6fa20cbe8d3c02be7d8
Swaroop C H <swaroop@swaroopch.com>
2012-06-23 11:52:08 +0530

    Updating Monger version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                            [org.clojure/data.json "0.1.3"]
                            [org.clojure/core.cache "0.5.0"]
                            [noir "1.3.0-beta7"]
-                           [com.novemberain/monger "1.0.0-beta6"]
+                           [com.novemberain/monger "1.0.0-rc2"]
                            [clj-time "0.4.2"]
                            [enlive "1.0.0"]
                            [stencil "0.3.0-preview1"]
